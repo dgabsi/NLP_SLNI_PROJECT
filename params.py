7c0e6d21b7ec62aa10f6973847b50121bb32dd2b
neuralx <daniela@neuralx.co.uk>
2021-04-10 08:33:02 +0100

    final corrections1

--- params.py
+++ params.py
@@ -8,10 +8,6 @@ RUNS_DIR='./experiments'
 RESULTS_DIR='./results'
 
 
-BEST_MODEL_BASIC_RNN='./saved_models/best_models/best_basic_rnn.th'
-BEST_MODEL_TRANSFORMER='./saved_models/best_models/best_basic_rnn.th'
-BEST_MODEL_RNN_COMBINE='./saved_models/best_models/best_basic_rnn.th'
-BEST_MODEL_BERT='./saved_models/best_models/best_basic_rnn.th'
 
 ##CONFIGURATION
 
@@ -35,29 +31,3 @@ TRANSFORMER_CONSTANT_CONFIG={'num_classes':3, 'max_len':1000, 'hidden_pre2_class
 
 
 
-
-'''
-
-#LOAD_VOC=True
-#MAX_LEN=512
-#EMBEDDING_SIZE=512
-#NUM_HEAD=4
-#DROPOUT_RATE=0.1
-#WEIGHT_DECAY=0.01
-
-#HIDDEN_SIZE=500
-
-BERT_CONFIG_LIST=[{'run_name': ' lr:5e-5', 'num_epochs':4, 'lr':5e-5, 'checkpoint': None}]#, {'run_name': 'lr 3e-5','num_epochs':4,'lr':3e-5}]
-
-RNN_CONFIG_LIST=[{'run_name': 'lr:0.003 bs:32 embedding: glove','num_epochs':4, 'lr':0.003, 'batch_size':BATCH_SIZE, 'embedding_type': 'glove', 'checkpoint': None}]
-
-#RNN_WITH_ATTN_CONFIG_LIST=[{'run_name': ' last1 lr 0.001','num_epochs':1, 'lr':0.001}]
-
-RNN_COMBINE_CONFIG_LIST=[{'run_name': 'lr:0.0003 bs:32 embedding: glove','num_epochs':4, 'lr':0.0003, 'batch_size':BATCH_SIZE, 'embedding_type': 'glove', 'checkpoint': None}]
-
-
-TRANSFORMER_CONFIG_LIST=[{'run_name': 'lr:5e-5 bs:32 embedding: glove', 'num_epochs':2, 'lr':5e-5, 'batch_size':BATCH_SIZE, 'embedding_type':'glove', 'checkpoint': 'Transformerlr:5e-5 bs:32 embedding: glove  epoch:5 train_iter:85840 val_iter:154003202021 07.pth'}]
-
-#{'run_name': ' lr 5e-5','num_epochs':5, 'lr':5e-5}
-
-'''
